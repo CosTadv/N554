4d35cde3cf220afeb2ce3f855db64ca1bd4caaae
Davi Costa <dvcosta0505@outlook.com>
2017-02-10 15:02:19 -0300

    Exercicio 01

---
+++ front-end/js/DisciplinaController.js
@@ -0,0 +1,16 @@
+app.controller('DisciplinaController', function($scope) {
+
+	//$scope.disciplina = {};
+	$scope.listaDisciplinas = [];
+
+	$scope.gravar = function() {
+		$scope.listaDisciplinas.push($scope.disciplina);
+		$scope.disciplina = {};
+	}
+
+	$scope.remover = function(disciplina) {
+		$scope.listaDisciplinas.splice($scope.listaDisciplinas.indexOf(disciplina),1);
+	}
+
+
+});
